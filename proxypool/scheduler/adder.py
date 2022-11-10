e005fa70a2e1e8ebf88ab2c2d8315e1ed851f70e
Jean Christophe <6159984@gmail.com>
2022-11-10 11:30:25 +0800

    reformat code

--- proxypool/scheduler/adder.py
+++ proxypool/scheduler/adder.py
@@ -44,7 +44,7 @@ class PoolAdder:
         while not self.is_over():
 
             flag += 1
-            Tool.info(f'Start add proxies for the {flag}th time')
+            Tool.info(f'Start add proxies for the {flag}th time...')
             raw_proxies = []
             with futures.ThreadPoolExecutor(max_workers=len(spiders)) as executor:
                 future_to_down = [executor.submit(spiders[i].get, 5) for i in range(len(spiders))]
@@ -54,12 +54,14 @@ class PoolAdder:
 
             self._counter.set_before_number(raw_proxies)
 
+            # Below is the interface of filtering proxies
             self._tester.set_raw_proxies(raw_proxies)
             self._tester.filter()
             proxies = self._tester.usable_proxies
 
             self._counter.set_after_number(proxies)
             self._counter.report()
+
             if len(proxies) != 0:
                 self._pool.puts(proxies)
             if self.is_over():
